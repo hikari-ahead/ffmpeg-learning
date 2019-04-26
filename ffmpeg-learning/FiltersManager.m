01b119372a7e838d9b7444908e81df4657523033
Kuangjeon Tian <tianguangqian@bytedance.com>
2019-04-26 20:08:16 +0800

    filter test

--- ffmpeg-learning/FiltersManager.m
+++ ffmpeg-learning/FiltersManager.m
@@ -10,7 +10,11 @@
 #import "avutil.h"
 #import "avfilter.h"
 #import "avcodec.h"
-#import <libavformat/avformat.h>
+#import "opt.h"
+#import "buffersrc.h"
+#import "buffersink.h"
+#import "avformat.h"
+#import "swscale.h"
 
 
 static AVFormatContext *fmt_ctx;
@@ -22,12 +26,14 @@ static int video_stream_index = -1;
 static int64_t last_pts = AV_NOPTS_VALUE;
 
 @interface FiltersManager () {
-    AVFilterGraph *filter_graph;
-    AVFilterContext *filter_context;
-    AVFilter *filter;
+//    AVFilterGraph *filter_graph;
+//    AVFilterContext *filter_context;
+//    AVFilter *filter;
 }
 @end
 
+
+const char *filter_descr = "scale=78:24,transpose=cclock";
 static int open_input_file(const char *filename)
 {
     int ret;
@@ -66,6 +72,202 @@ static int open_input_file(const char *filename)
     return 0;
 }
 
+static int init_filters(const char *filters_descr)
+{
+    char args[512];
+    int ret = 0;
+    const AVFilter *buffersrc  = avfilter_get_by_name("buffer");
+    const AVFilter *buffersink = avfilter_get_by_name("buffersink");
+    AVFilterInOut *outputs = avfilter_inout_alloc();
+    AVFilterInOut *inputs  = avfilter_inout_alloc();
+    AVRational time_base = fmt_ctx->streams[video_stream_index]->time_base;
+    enum AVPixelFormat pix_fmts[] = { AV_PIX_FMT_GRAY8, AV_PIX_FMT_NONE };
+
+    filter_graph = avfilter_graph_alloc();
+    if (!outputs || !inputs || !filter_graph) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
+    /* buffer video source: the decoded frames from the decoder will be inserted here. */
+    snprintf(args, sizeof(args),
+             "video_size=%dx%d:pix_fmt=%d:time_base=%d/%d:pixel_aspect=%d/%d",
+             dec_ctx->width, dec_ctx->height, dec_ctx->pix_fmt,
+             time_base.num, time_base.den,
+             dec_ctx->sample_aspect_ratio.num, dec_ctx->sample_aspect_ratio.den);
+
+    ret = avfilter_graph_create_filter(&buffersrc_ctx, buffersrc, "in",
+                                       args, NULL, filter_graph);
+    if (ret < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot create buffer source\n");
+        goto end;
+    }
+
+    /* buffer video sink: to terminate the filter chain. */
+    ret = avfilter_graph_create_filter(&buffersink_ctx, buffersink, "out",
+                                       NULL, NULL, filter_graph);
+    if (ret < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot create buffer sink\n");
+        goto end;
+    }
+
+    ret = av_opt_set_int_list(buffersink_ctx, "pix_fmts", pix_fmts,
+                              AV_PIX_FMT_NONE, AV_OPT_SEARCH_CHILDREN);
+    if (ret < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot set output pixel format\n");
+        goto end;
+    }
+
+    /*
+     * Set the endpoints for the filter graph. The filter_graph will
+     * be linked to the graph described by filters_descr.
+     */
+
+    /*
+     * The buffer source output must be connected to the input pad of
+     * the first filter described by filters_descr; since the first
+     * filter input label is not specified, it is set to "in" by
+     * default.
+     */
+    outputs->name       = av_strdup("in");
+    outputs->filter_ctx = buffersrc_ctx;
+    outputs->pad_idx    = 0;
+    outputs->next       = NULL;
+
+    /*
+     * The buffer sink input must be connected to the output pad of
+     * the last filter described by filters_descr; since the last
+     * filter output label is not specified, it is set to "out" by
+     * default.
+     */
+    inputs->name       = av_strdup("out");
+    inputs->filter_ctx = buffersink_ctx;
+    inputs->pad_idx    = 0;
+    inputs->next       = NULL;
+
+    if ((ret = avfilter_graph_parse_ptr(filter_graph, filters_descr,
+                                        &inputs, &outputs, NULL)) < 0)
+        goto end;
+
+    if ((ret = avfilter_graph_config(filter_graph, NULL)) < 0)
+        goto end;
+
+end:
+    avfilter_inout_free(&inputs);
+    avfilter_inout_free(&outputs);
+
+    return ret;
+}
+
+static void display_frame(const AVFrame *frame, AVRational time_base)
+{
+    int x, y;
+    uint8_t *p0, *p;
+    int64_t delay;
+
+    if (frame->pts != AV_NOPTS_VALUE) {
+        if (last_pts != AV_NOPTS_VALUE) {
+            /* sleep roughly the right amount of time;
+             * usleep is in microseconds, just like AV_TIME_BASE. */
+            delay = av_rescale_q(frame->pts - last_pts,
+                                 time_base, AV_TIME_BASE_Q);
+            if (delay > 0 && delay < 1000000)
+                usleep(delay);
+        }
+        last_pts = frame->pts;
+    }
+
+    /* Trivial ASCII grayscale display. */
+    p0 = frame->data[0];
+    puts("\033c");
+    for (y = 0; y < frame->height; y++) {
+        p = p0;
+        for (x = 0; x < frame->width; x++)
+            putchar(" .-+#"[*(p++) / 52]);
+        putchar('\n');
+        p0 += frame->linesize[0];
+    }
+    fflush(stdout);
+}
+
+AVFrame *internal_main(char *filePath)
+{
+    int ret;
+    AVPacket packet;
+    AVFrame *frame;
+    AVFrame *filt_frame;
+
+    frame = av_frame_alloc();
+    filt_frame = av_frame_alloc();
+    if (!frame || !filt_frame) {
+        perror("Could not allocate frame");
+        exit(1);
+    }
+
+    if ((ret = open_input_file(filePath)) < 0)
+        goto end;
+    if ((ret = init_filters(filter_descr)) < 0)
+        goto end;
+
+    /* read all packets */
+    while (1) {
+        if ((ret = av_read_frame(fmt_ctx, &packet)) < 0)
+            break;
+
+        if (packet.stream_index == video_stream_index) {
+            ret = avcodec_send_packet(dec_ctx, &packet);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Error while sending a packet to the decoder\n");
+                break;
+            }
+
+            while (ret >= 0) {
+                ret = avcodec_receive_frame(dec_ctx, frame);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+                    break;
+                } else if (ret < 0) {
+                    av_log(NULL, AV_LOG_ERROR, "Error while receiving a frame from the decoder\n");
+                    goto end;
+                }
+
+                frame->pts = frame->best_effort_timestamp;
+
+                /* push the decoded frame into the filtergraph */
+                if (av_buffersrc_add_frame_flags(buffersrc_ctx, frame, AV_BUFFERSRC_FLAG_KEEP_REF) < 0) {
+                    av_log(NULL, AV_LOG_ERROR, "Error while feeding the filtergraph\n");
+                    break;
+                }
+
+                /* pull filtered frames from the filtergraph */
+                while (1) {
+                    ret = av_buffersink_get_frame(buffersink_ctx, filt_frame);
+                    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+                        break;
+                    if (ret < 0)
+                        goto end;
+                    display_frame(filt_frame, buffersink_ctx->inputs[0]->time_base);
+//                    av_frame_unref(filt_frame);
+                }
+                av_frame_unref(frame);
+            }
+        }
+        av_packet_unref(&packet);
+    }
+end:
+    avfilter_graph_free(&filter_graph);
+    avcodec_free_context(&dec_ctx);
+    avformat_close_input(&fmt_ctx);
+    av_frame_free(&frame);
+//    av_frame_free(&filt_frame);
+
+    if (ret < 0 && ret != AVERROR_EOF) {
+        fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+    }
+    return filt_frame;
+}
+
+
+
 @implementation FiltersManager
 - (instancetype)init {
     self = [super init];
@@ -77,7 +279,90 @@ static int open_input_file(const char *filename)
 
 - (void)commonSetup {
     NSString *path = [NSBundle.mainBundle pathForResource:@"a5" ofType:@"jpg"];
-    open_input_file(path.UTF8String);
+    AVFrame *frame = internal_main(path.UTF8String);
+    UIImage *image = [self converUIImageFromAVFrame:frame];
+    free(frame);
+}
+
+- (CVPixelBufferRef)converCVPixelBufferRefFromAVFrame:(AVFrame *)avframe {
+    if (!avframe || !avframe->data[0]) {
+        return NULL;
+    }
+
+    CVPixelBufferRef outputPixelBuffer = NULL;
+
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+
+                             @(avframe->linesize[0]), kCVPixelBufferBytesPerRowAlignmentKey,
+                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESCompatibilityKey,
+                             [NSDictionary dictionary], kCVPixelBufferIOSurfacePropertiesKey,
+                             nil];
+
+
+    if (avframe->linesize[1] != avframe->linesize[2]) {
+        return  NULL;
+    }
+
+    size_t srcPlaneSize = avframe->linesize[1]*avframe->height/2;
+    size_t dstPlaneSize = srcPlaneSize *2;
+    uint8_t *dstPlane = malloc(dstPlaneSize);
+
+    // interleave Cb and Cr plane
+    for(size_t i = 0; i<srcPlaneSize; i++){
+        dstPlane[2*i  ]=avframe->data[1][i];
+        dstPlane[2*i+1]=avframe->data[2][i];
+    }
+
+
+    int ret = CVPixelBufferCreate(kCFAllocatorDefault,
+                                  avframe->width,
+                                  avframe->height,
+                                  kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange,
+                                  (__bridge CFDictionaryRef)(options),
+                                  &outputPixelBuffer);
+
+    CVPixelBufferLockBaseAddress(outputPixelBuffer, 0);
+
+    size_t bytePerRowY = CVPixelBufferGetBytesPerRowOfPlane(outputPixelBuffer, 0);
+    size_t bytesPerRowUV = CVPixelBufferGetBytesPerRowOfPlane(outputPixelBuffer, 1);
+
+    void* base =  CVPixelBufferGetBaseAddressOfPlane(outputPixelBuffer, 0);
+    memcpy(base, avframe->data[0], bytePerRowY*avframe->height);
+
+    base = CVPixelBufferGetBaseAddressOfPlane(outputPixelBuffer, 1);
+    memcpy(base, dstPlane, bytesPerRowUV*avframe->height/2);
+
+    CVPixelBufferUnlockBaseAddress(outputPixelBuffer, 0);
+
+    free(dstPlane);
+
+    if(ret != kCVReturnSuccess)
+    {
+        NSLog(@"CVPixelBufferCreate Failed");
+        return NULL;
+    }
+
+    return outputPixelBuffer;
+}
+
+- (UIImage *)converUIImageFromAVFrame:(AVFrame*)frame {
+    CVPixelBufferRef pixelBuffer = [self converCVPixelBufferRefFromAVFrame:frame];
+
+    CIImage *ciImage = [CIImage imageWithCVPixelBuffer:pixelBuffer];
+
+    CIContext *temporaryContext = [CIContext contextWithOptions:nil];
+    CGImageRef videoImage = [temporaryContext
+                             createCGImage:ciImage
+                             fromRect:CGRectMake(0, 0,
+                                                 CVPixelBufferGetWidth(pixelBuffer),
+                                                 CVPixelBufferGetHeight(pixelBuffer))];
+
+    UIImage *uiImage = [UIImage imageWithCGImage:videoImage];
+
+    CVPixelBufferRelease(pixelBuffer);
+    CGImageRelease(videoImage);
+
+    return uiImage;
 }
 
 @end
